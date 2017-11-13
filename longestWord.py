450f2fbf0ffc2c31628782577c9bdb2b648fb3c2
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-13 14:46:57 -0500

    Updated from Brython Server: 11/13/2017 2:46:56 PM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,11 @@
+#Clay Kynor
+#11/13/17
+#middleWord.py - finds the middle word
 
+word = input('Enter a list of words: ').split(' ')
+for w in word:
+    total = 0
+    if len(w)>total:
+        total = w
+        
+print(total)
