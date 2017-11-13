c0afe48c38c317f920cc60001e93a6069f902aa1
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-13 14:27:00 -0500

    Updated from Brython Server: 11/13/2017 2:26:59 PM

--- listDemo.py
+++ listDemo.py
@@ -1,1 +1,10 @@
+#Clay Kynor
+#11/13/17
+#listDemo.py - print out first and last words in list
 
+words = input('Enter some words: ').split(' ')
+
+#print out list one item for line
+
+for w in words:
+    print(w)
