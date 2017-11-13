0b2b42faf762d86e6a90e8208c92c300380eef32
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-13 14:43:15 -0500

    Updated from Brython Server: 11/13/2017 2:43:14 PM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,10 @@
+#Clay Kynor
+#11/13/17
+#middleWord.py - finds the middle word
 
+words = input('Enter a list of words: ').split(' ')
+length = (len(words))
+if length%2==0:
+    print("The middle word(s): ", words[(length//2-1):((length//2+1))])
+else:
+    print("The middle word(s): ", words[(length//2)])
