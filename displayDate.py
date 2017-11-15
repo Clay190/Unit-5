e2ea8e14e44bbceb7f45875dbf8ddc45eb38d4af
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-15 13:59:35 -0500

    Updated from Brython Server: 11/15/2017 1:59:35 PM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,17 @@
+#Clay Kynor
+#11/15/17
+#displayDate.py
 
+from daytime import *
+
+months = ['January', 'February', "March", 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+weekday= ['Monday', 'Tuesday', 'Wensday', 'Thursday', 'Firday', 'Saturday', 'Sunday']
+
+day = date.today
+month = date.month
+year = date.year
+
+print(today)
+print(day)
+print(year)
+print(month)
