dc2bd2527ce9ebc85e3e92f505be8307e0739231
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-16 14:54:19 -0500

    Updated from Brython Server: 11/16/2017 2:54:18 PM

--- stat.py
+++ stat.py
@@ -1,1 +1,20 @@
+#Clay Kynor
+#11/15/17
+#stat.py - finding stats
 
+numbers = []
+while True:
+    num = input("Enter a number or press 'q' when finished ")
+    if num == 'q':
+        break  
+    numbers.append(float(num))
+
+numbers.sort()
+
+times = numbers.count()
+
+print("Min: ", min(numbers))
+print("Max: ", max(numbers))
+print("Mean: ", sum(numbers)//len(numbers))
+print("Mode: ")
+print("Median: ", numbers[len(numbers)//2,(len(numbers)//2)+1])
