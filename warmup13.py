bae23412bca3a2a6843e123727709eff49ef8e87
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-16 14:25:25 -0500

    Updated from Brython Server: 11/16/2017 2:25:24 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,17 @@
+#Clay Kynor
+#11/16/17
+#warmup13.py
 
+from random import randint
+
+words = []
+
+for i in range(1,21):
+    num = randint(1,100)
+    words.append(num)
+   
+words.sort()
+print(words)
+print("The maximum is", words[19])
+print("The minimum is", words[0])
+print("Sum", sum(words))
