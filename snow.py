3ed04fab9224ae87b919c49c2ad66887febbeb7d
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-20 14:50:48 -0500

    Updated from Brython Server: 11/20/2017 2:50:47 PM

--- snow.py
+++ snow.py
@@ -1,1 +1,18 @@
+#Clay Kynor
+#11/20/17
+#snow.py - making snow fall from the sky 
 
+from ggame import *
+from random import randint
+
+WIDTH = 100
+HEIGHT = 100
+SNOW_SIZE = 5
+
+
+black = Color(0x000000,1)
+backround = RectangleAsset(WIDTH,HEIGHT,LineStyle(1,black),black)
+
+
+Sprite(backround)
+App().run()
