65f1dd975c63b2785214e3d5c0097ac356c081fe
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-11-30 14:14:57 -0500

    Updated from Brython Server: 11/30/2017 2:14:56 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,14 @@
+#Clay Kynor
+#11/30/17
+#warmup16.py - Write a function that takes a list of numbers as an argument and returns another list where each number is doubled
 
+
+
+def double():
+    L = [1,3,6,12,33]
+    num = len(L)
+    for i in range(0,num):
+        L[i] = L[i]*2
+    return L
+        
+print(double())
