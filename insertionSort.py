28de99ffe21d2c2c29930e190f4a1ee2512ff750
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-01 14:23:09 -0500

    Create insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,58 @@
+#Clay Kynor
+#11/29/17
+#insertionSort.py - making a sorting algorithm
+
+from random import randint
+from time import time
+
+N = 100 #how many numbers will be sorted
+
+'''
+procedure bubbleSort( A : list of sortable items )
+    n = length(A)
+    repeat 
+        swapped = false
+        for i = 1 to n-1 inclusive do
+            /* if this pair is out of order */
+            if A[i-1] > A[i] then
+                /* swap them and remember something changed */
+                swap( A[i-1], A[i] )
+                swapped = true
+            end if
+        end for
+    until not swapped
+end procedure'''
+
+def mySort(L):
+    n = len(L)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1,n):
+            if L[i-1] > L[i]:
+                L[i], L[i-1] = L[i-1], L[i]
+                swapped = True
+    return L
+
+if __name__ == '__main__':
+    
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+        
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+       
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
+    
+
